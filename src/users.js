cf6caf3e2739b83b547c542a8684e2ac02a714a2
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-01-14 00:09:16 +0100

    added a filter by letter function

--- src/users.js
+++ src/users.js
@@ -6,6 +6,8 @@ function Users({ title, searchTitle }) {
     const [users, setUsers] = useState([]);
     const [nameToSearch, setNameToSearch] = useState('')
     const [filteredUsers, setFilteredUsers] = useState([]);
+    const [filterLetter, setFilterLetter] = useState('');
+    const [filteredUsersByLetter, setFilteredUsersByLetter] = useState([])
 
     const fetchData = () => {
         try {
@@ -34,6 +36,13 @@ function Users({ title, searchTitle }) {
         }
     }
 
+    const handleFilter = (e) => {
+        const letter = e.target.value
+        setFilterLetter(letter)
+        const alphabetFiltered = users.filter((user) => user.name[0].toUpperCase() === letter)
+        setFilteredUsersByLetter(alphabetFiltered)
+    }
+
 
     // const KEYS_TO_FILTERS = ['name'];
 
@@ -47,13 +56,41 @@ function Users({ title, searchTitle }) {
                 {searchTitle}
                 <input id='search' type='text' placeholder='Enter a name to search' onChange={handleSearch} value={nameToSearch} />
             </label>
+            <label htmlFor="filter">
+                Filter
+            </label>
+            <select id="filter" name="alphabeticalFilter" onChange={handleFilter} value={filterLetter}>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+                <option value="G">G</option>
+                <option value="H">H</option>
+                <option value="I">I</option>
+                <option value="J">J</option>
+                <option value="K">K</option>
+                <option value="L">L</option>
+                <option value="M">M</option>
+                <option value="N">N</option>
+                <option value="O">O</option>
+                <option value="P">P</option>
+                <option value="Q">Q</option>
+                <option value="R">R</option>
+                <option value="S">S</option>
+                <option value="T">T</option>
+                <option value="U">U</option>
+                <option value="V">V</option>
+                <option value="W">W</option>
+                <option value="X">X</option>
+                <option value="Y">Y</option>
+                <option value="Z">Z</option>
+            </select>
             <h1>{title}</h1>
             <ul>
-                {nameToSearch.length === 0
-                    ? users.map((user) => <li key={user.id}>{user.name}</li>)
-                    : filteredUsers.length > 0
-                        ? filteredUsers.map((user) => <li key={user.id}>{user.name}</li>)
-                        : <li>No Users Found!</li>
+                {
+                    filteredUsersByLetter.map((user) => <li key={user.id}>{user.name}</li>)
                 }
             </ul>
         </>
